e4d840ffe2f1f97ec61365bd97c71758356bf64c
ArthurBondar <arthur.bondar.1@gmail.com>
2025-03-29 21:30:53 -0400

    contact us page added

---
+++ travel_recommendation.js
@@ -0,0 +1,11 @@
+
+
+function loadSuggestions() {
+    const input = document.getElementById('conditionInput').value.toLowerCase();
+    const resultDiv = document.getElementById('result');
+    resultDiv.innerHTML = '';
+
+
+
+
+}
